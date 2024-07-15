e57d34bdbd02b365259887cd8c591243991feec6
Faokushi <aspog0099@gmail.com>
2024-07-16 01:08:21 +0800

    asdasd

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -15,12 +16,224 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final TextEditingController firstNumberController = TextEditingController();
+  final TextEditingController secondNumberController = TextEditingController();
+
+  @override
+  void dispose() {
+    firstNumberController.dispose();
+    secondNumberController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Assignemnt"),
+        backgroundColor: Colors.blue,
+        ),
+      body: Container(
+        color: Colors.yellow,
+        child: Center(child: 
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              InputFirstNumber(myController: firstNumberController),
+              const SizedBox(height: 10),
+              InputSecondNumber(myController: secondNumberController),
+              const SizedBox(height: 10),
+               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  plusButton(x: int.parse(firstNumberController.text),y: int.parse(secondNumberController.text)),
+                  const SizedBox(width: 10,),
+                  MinusButton(x: int.parse(firstNumberController.text),y: int.parse(secondNumberController.text)),
+                  const SizedBox(width: 10,),
+                  DivideButton(x: int.parse(firstNumberController.text),y: int.parse(secondNumberController.text)),
+                  const SizedBox(width: 10,),
+                  MultiplyButton(x: int.parse(firstNumberController.text),y: int.parse(secondNumberController.text)),
+                ],),
+              const Text(
+                "RESULT: ",
+                style: TextStyle(fontSize: 50),
+                )
+              ]
+            ),
+          )
+        ),
+
+    );
+  }
+}
+
+class InputFirstNumber extends StatefulWidget {
+  const InputFirstNumber({super.key, required this.myController});
+  final TextEditingController myController;
+
+  @override
+  State<InputFirstNumber> createState() => _InputFirstNumberState();
+}
+
+class _InputFirstNumberState extends State<InputFirstNumber> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: 200.0, 
+        child: 
+          TextField(
+            controller: widget.myController,            
+            textAlign: TextAlign.center, 
+            keyboardType: const TextInputType.numberWithOptions(decimal: true) ,
+            decoration: const InputDecoration(alignLabelWithHint: null, hintText: "First Number",border: OutlineInputBorder()),
+        ),
+      ),
+    );
+  }
+}
+
+class InputSecondNumber extends StatefulWidget {
+  const InputSecondNumber({super.key, required this.myController});
+  final TextEditingController myController;
+
+  @override
+  State<InputSecondNumber> createState() => _InputSecondNumberState();
+}
+
+class _InputSecondNumberState extends State<InputSecondNumber> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      
+      child: SizedBox(
+        width: 200.0, 
+        child: 
+          TextField(
+            controller: widget.myController,
+            textAlign: TextAlign.center, 
+            keyboardType: const TextInputType.numberWithOptions(decimal: true) ,
+            decoration: const InputDecoration(alignLabelWithHint: null, hintText: "Second Number",border: OutlineInputBorder()),
+        ),
+      ),
+    );
+  }
+}
+
+class plusButton extends StatefulWidget {
+  const plusButton({super.key, required this.x, required this.y});
+  final int x;
+  final int y;
+  
+
+  @override
+  State<plusButton> createState() => _plusButtonState();
+}
+
+class _plusButtonState extends State<plusButton> {
+
+  void plusButton() {
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(50, 50),
+        textStyle: const TextStyle(fontSize: 20),
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.black
+      ), onPressed: () {  }, 
+      child: const Text("+"),);
+  }
+}
+
+class MinusButton extends StatefulWidget {
+  const MinusButton({super.key, required this.x, required this.y});
+  final int x;
+  final int y;
+
+  @override
+  State<MinusButton> createState() => _MinusButtonState();
+}
+
+class _MinusButtonState extends State<MinusButton> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(50, 50),
+        textStyle: const TextStyle(fontSize: 20),
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.black
+      ), onPressed: () {  }, 
+      child: const Text("-"),);
+  }
+}
+
+class DivideButton extends StatefulWidget {
+  const DivideButton({super.key, required this.x, required this.y});
+  final int x;
+  final int y;
+
+  @override
+  State<DivideButton> createState() => _DivideButtonState();
+}
+
+class _DivideButtonState extends State<DivideButton> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(50, 50),
+        textStyle: const TextStyle(fontSize: 20),
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.black
+      ), onPressed: () {  }, 
+      child: const Text("/"),);
+  }
+}
+
+class MultiplyButton extends StatefulWidget {
+  const MultiplyButton({super.key, required this.x, required this.y});
+  final int x;
+  final int y;
+
+  @override
+  State<MultiplyButton> createState() => _MultiplyButtonState();
+}
+
+class _MultiplyButtonState extends State<MultiplyButton> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(50, 50),
+        textStyle: const TextStyle(fontSize: 20),
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.black
+      ), onPressed: () {  }, 
+      child: const Text("x"),);
+  }
+}
+
+/*class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
   @override
   Widget build(BuildContext context) {
@@ -46,16 +259,10 @@ class MyHomePage extends StatelessWidget {
               ],
             )));
   }
-}
+}*/
 
-class TextInput extends StatefulWidget {
-  const TextInput({super.key});
 
-  @override
-  State<TextInput> createState() => _TextInputState();
-}
-
-class _TextInputState extends State<TextInput> {
+/*class _TextInputState extends State<TextInput> {
   final TextEditingController controller = TextEditingController();
   String text2 = "";
   @override
@@ -91,4 +298,4 @@ class _TextInputState extends State<TextInput> {
       ],
     );
   }
-}
+}*/
